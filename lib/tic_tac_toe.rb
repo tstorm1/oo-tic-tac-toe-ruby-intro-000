51cef2eff956311542a5252ffb95b398c1f62857
Tom Storm <tstorm@invoca.com>
2018-04-30 01:21:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    if index.between?(0,8) && position_taken?(@board, index) == false
+    if index.between?(0,8) && position_taken?(index) == false
       return true
     else return false
     end
